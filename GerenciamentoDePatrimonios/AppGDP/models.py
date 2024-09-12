b4fcd4840cd9f0dcefa1345d4aceef316f993a09
Guilherme-Augusto06 <ga375464@gmail.com>
2024-09-12 09:02:17 -0300

    feat: Adicionando model de usuário com nome, nif, sala, email e senha

--- GerenciamentoDePatrimonios/AppGDP/models.py
+++ GerenciamentoDePatrimonios/AppGDP/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 class Usuario(models.Model):
     nome = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
+    nif = models.CharField(max_length=8)
+    sala = models.CharField(max_length=20)
+    email = models.EmailField(max_length=80)
+    senha = models.CharField(max_length=20)
     
