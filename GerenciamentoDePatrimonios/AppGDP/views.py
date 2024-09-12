2d6fb5fb55cc6dfefa2316e25785157a8c7cb996
Guilherme-Augusto06 <ga375464@gmail.com>
2024-09-12 09:05:20 -0300

    feat: adicionando view para Login ser redirecionado para welcomeHomepage

--- GerenciamentoDePatrimonios/AppGDP/views.py
+++ GerenciamentoDePatrimonios/AppGDP/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from AppGDP.models import Senai
+from AppGDP.forms import FormLogin
 
 # Create your views here.
 
@@ -25,3 +30,30 @@ def welcomeHomepage(request):
 
 def itens(request):
     return render(request, 'itens.html')
+
+def login(request):
+    context = {}
+    dadosSenai = Senai.objects.all()
+    context["dadosSenai"] = dadosSenai
+    if request.method == 'POST':
+        form = FormLogin(request.POST)
+        if form.is_valid():
+            var_usuario = form.cleaned_data['user']
+            var_senha = form.cleaned_data['password']
+            
+            user = authenticate(username=var_usuario, password=var_senha)
+
+            if user is not None:
+                auth_login(request, user)
+                if user.groups.filter(name='Coordenador').exists():
+                    return redirect('/homepageAdmin')
+                elif user.groups.filter(name='Professor').exists():
+                    return redirect('/homepageProfessor')
+                else:
+                    return redirect('/')
+            else:
+                print('Login falhou')
+    else:
+        form = FormLogin()
+        context['form'] = form
+    return render(request, 'login.html', context)
