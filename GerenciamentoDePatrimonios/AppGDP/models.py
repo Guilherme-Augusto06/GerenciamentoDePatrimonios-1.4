d10937b2e0a5cea25960b0439cb7f4f2bc87f9c2
Guilherme-Augusto06 <ga375464@gmail.com>
2024-09-12 09:03:22 -0300

    feat: Aficionando a Model senai com titulo, descrição e logo para reutilizações futuras

--- GerenciamentoDePatrimonios/AppGDP/models.py
+++ GerenciamentoDePatrimonios/AppGDP/models.py
@@ -7,3 +7,7 @@ class Usuario(models.Model):
     email = models.EmailField(max_length=80)
     senha = models.CharField(max_length=20)
     
+class Senai(models.Model):
+    titulo = models.CharField(max_length=50)
+    descricao = models.TextField(max_length=1500)
+    logo = models.ImageField(upload_to='logo/')
