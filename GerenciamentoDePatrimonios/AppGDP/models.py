080065ac327acf47b7a6496a5b7b233b3d60b1b4
Guilherme-Augusto06 <ga375464@gmail.com>
2024-09-17 13:39:10 -0300

    feat: criação da tabela de inventario na models

--- GerenciamentoDePatrimonios/AppGDP/models.py
+++ GerenciamentoDePatrimonios/AppGDP/models.py
@@ -6,3 +6,16 @@ class Senai(models.Model):
     titulo = models.CharField(max_length=50)
     descricao = models.TextField(max_length=1500)
     logo = models.ImageField(upload_to='logo/')
+
+    def __str__(self):
+        return self.titulo
+    
+from django.db import models
+
+class Inventario(models.Model):
+    num_inventario = models.CharField(max_length=10, unique=True)
+    denominacao = models.CharField(max_length=255)
+    localizacao = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.num_inventario} - {self.denominacao}"
